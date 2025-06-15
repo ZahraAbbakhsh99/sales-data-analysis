5ade3d451f9376e1a02a443deeac88b69966d6d5
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-06-15 17:26:32 +0330

    Insert unique product subcategories with category mapping and update ID map

--- ETL.py
+++ ETL.py
@@ -73,10 +73,28 @@ for customer_id, customer_category in df[['customer_id', 'customer_category']].d
         cursor.execute("INSERT IGNORE INTO customers (id, category) VALUES (%s, %s)", (customer_id, customer_category))
         conn.commit()
         customer_map[customer_id] = customer_id
-        
+
 # Product Categories
 for product_category in df['product_category'].dropna().unique():
     if product_category not in product_category_map:
         insert_query = "INSERT IGNORE INTO products_category (name) VALUES (%s)"
         cat_id = insert_or_get_id('products_category', 'name', product_category, insert_query=insert_query, insert_values=(clean_value(product_category),))
         product_category_map[product_category] = cat_id
+
+# Product Subcategories
+subcat_unique = df[['product_sub_category', 'product_category']].drop_duplicates()
+for _, row in subcat_unique.iterrows():
+    sub_cat = row['product_sub_category']
+    cat_name = row['product_category']
+    if pd.isna(sub_cat) or not cat_name:
+        continue
+    key = (sub_cat, cat_name)
+    if key not in product_sub_category_map:
+        cat_id = product_category_map.get(cat_name)
+        if not cat_id:
+            continue
+        cursor.execute("INSERT IGNORE INTO product_sub_category (category_id, sub_category) VALUES (%s, %s)", (cat_id, sub_cat))
+        conn.commit()
+        cursor.execute("SELECT id FROM product_sub_category WHERE category_id=%s AND sub_category=%s", (cat_id, sub_cat))
+        subcat_id = cursor.fetchone()[0]
+        product_sub_category_map[key] = subcat_id
