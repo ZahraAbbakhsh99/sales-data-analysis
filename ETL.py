6dd6cd57ea5b219388a114935a21b603ed4de9a6
Arezoo <138774289+ZahraAbbakhsh99@users.noreply.github.com>
2025-06-15 17:10:40 +0330

    Add Excel import with functions to normalize Persian date and datetime

--- ETL.py
+++ ETL.py
@@ -1,4 +1,15 @@
 import mysql.connector
+import pandas as pd
+
+def normalize_persian_date(pdate):
+    if isinstance(pdate, str):
+        return pdate.replace('/', '-')
+    return pdate
+
+def normalize_datetime(row):
+    time_part, date_part = row['date_time'].split()
+    date_norm = normalize_persian_date(date_part)
+    return f"{date_norm} {time_part}:00"
 
 # DB Connection
 conn = mysql.connector.connect(
@@ -9,5 +20,8 @@ conn = mysql.connector.connect(
 )
 cursor = conn.cursor(buffered=True)
 
-
+# Read Excel
+df = pd.read_excel('data/sample_sale.xlsx')
+df['date'] = df['date'].apply(normalize_persian_date)
+df['date_time'] = df.apply(normalize_datetime, axis=1)
 
